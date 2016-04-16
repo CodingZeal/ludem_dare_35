b191038f9280b953b2b09e440be0c5bb0d3609ea
Sean Culver <pair+sean@codingzeal.com>
2016-04-15 19:28:45 -0700

    Proof of Concept Idea

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public float speed = 10.0F;
+	void Update() {
+		float translation = Input.GetAxis ("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		transform.Translate (translation, 0, 0);
+	}
+}
