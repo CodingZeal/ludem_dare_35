49b935b901bb85656bba4513c39fe26dad6175ea
Sean Culver <pair+sean@codingzeal.com>
2016-04-16 15:05:19 -0700

    Add Player morphing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,6 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 
-	private Rigidbody rb;
-
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody> ();
-	}
-
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -20,20 +13,6 @@ public class PlayerController : MonoBehaviour
 
 		transform.position = new Vector3 (transform.position.x + moveHorizontal * Time.deltaTime, transform.position.y);
 
-		//Vector3 movement = new Vector3 (-moveHorizontal, 0.0f, 0.0f);
-//		
-		//transform.position (movement);
-
-		//Translate(movement * Time.deltaTime);
-
-//		rb.position = new Vector3 
-//			(
-////				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
-//				rb.position.x,
-//				0.0f, 
-//				rb.position.z
-//			);
-//
 	}
 }
 
---
+++ Assets/Scripts/PlayerMorph.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMorph : MonoBehaviour {
+
+	public Mesh[] shapes;
+
+	private int currentMeshIndex = 0;
+	private int[] randomIndex;
+	private Mesh currentMesh;
+	private MeshFilter currentMeshFilter;
+
+	void Start(){
+		currentMeshFilter = GetComponent<MeshFilter> ();
+		currentMesh = currentMeshFilter.mesh;
+	}
+		
+	void Update () {
+		if(Input.GetKeyDown("1")){
+			currentMeshFilter.mesh = shapes [0];
+		}
+		if(Input.GetKeyDown("2")){
+			currentMeshFilter.mesh = shapes [1];
+		}
+		if(Input.GetKeyDown("3")){
+			currentMeshFilter.mesh = shapes [2];
+		}
+		if(Input.GetKeyDown("4")){
+			currentMeshFilter.mesh = shapes [3];
+		}
+		if(Input.GetKeyDown("space")){
+			int randomIndex = RandomExcept (0, shapes.Length, currentMeshIndex);
+			currentMeshIndex = randomIndex;
+			currentMeshFilter.mesh = shapes [randomIndex];
+		}
+	}
+
+	public int RandomExcept ( int fromNr, int exclusiveToNr, int exceptNr ) {
+		int randomNr = exceptNr;
+
+		while ( randomNr == exceptNr ) {
+			randomNr = Random.Range( fromNr, exclusiveToNr );
+		}
+
+		return randomNr;
+	}
+}
