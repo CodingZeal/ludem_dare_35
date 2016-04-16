750c4abecc6d9fd051b515d9d0de8e6d2b9245dc
TastyOishi <tastykent@gmail.com>
2016-04-16 23:16:36 +1200

    Added 3D objects, and some fun images
    Signed-off-by: TastyOishi <tastykent@gmail.com>

--- Assets/Scripts/flash_light.cs
+++ Assets/Scripts/flash_light.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class flash_light : MonoBehaviour {
 
+
+	public Light lt;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () { 
+		lt = GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var BPM = Engine.bpm;
+		var minute = 6000;
+		if (Time.time /100 % (minute / BPM) == 0) {
+			lt.intensity = 8;
+		} else {
+			lt.intensity = 0;
+		}
 	}
 }
