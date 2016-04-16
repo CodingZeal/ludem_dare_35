83a6a0564cea4958d240a46b929d9cc445ecba57
Jason Harrison <jason@harrisonis.com>
2016-04-16 08:53:57 -0700

    infinite track and left-right player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Debug.Log ("Horizontal: " + moveHorizontal);
+		Debug.Log ("Vertical: " + moveVertical);
+
+		Vector3 movement = new Vector3 (-moveHorizontal, 0.0f, 0.0f);
+//		rb.velocity = movement * speed;
+		rb.transform.Translate(movement);
+
+		Debug.Log ("position:" + rb.position);
+
+//		rb.position = new Vector3 
+//			(
+////				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+//				rb.position.x,
+//				0.0f, 
+//				rb.position.z
+//			);
+//
+	}
+}
+
