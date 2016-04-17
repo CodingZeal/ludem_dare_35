c104ebe20b6a951a7a7c1aac4a21356aa5ae57bc
Sean Culver <pair+sean@codingzeal.com>
2016-04-16 17:18:26 -0700

    Create boundary and crash scripts

---
+++ Assets/Crash.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crash : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		print (collision.gameObject);
+	}
+}
---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		other.gameObject.transform.position = new Vector3 (0, 1, 0);
+	}
+}
